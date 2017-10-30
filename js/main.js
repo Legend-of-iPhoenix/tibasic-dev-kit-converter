d9ca2c914b7ecf943653f48998c15417c5282856
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-30 17:51:42 -0400

    Update main.js
    Added main functionality, and support for future functionality.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-//wip
+function submitDirtyCode() {
+	var code = document.getElementById("dirty-code").value;
+  
+  if (code != '') {
+    var input = document.getElementsByTagName("input");
+    input[0].submit();
+    convertCode(code);
+  }
+  else
+  {
+    alert("please gimme code; I'm hungry :P");
+  }
+}
+
+function convertCode(codeToConvert) {
+  document.getElementByTagName("output").value = detectFormatGraphScreen(codeToConvert);
+}
+
+function detectFormatGraphScreen(code)
+{
+      var fixedCode = code.replace(/1:prgmthetaFMT/g,"RectGC:CoordOn:GridOff:AxesOn Black:LabelOff:ExprOn:BorderColor 1:DetectAsymOn");
+      fixedCode = fixedCode.replace(/2:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4");
+      fixedCode = fixedCode.replace(/3:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4:ZQuadrant1::264->Xmax:164->Ymax");
+      return fixedCode;
+}
+
+/*
+function detectClearVars(code) {
+  var fixedCode = code.replace(/prgmCLR0VAR/g,"DelVar ADelVar BDelVar CDelVar DDelVar EDelVar FDelVar GDelVar HDelVar IDelVar JDelVar KDelVar LDelVar MDelVar NDelVar ODelVar QDelVar RDelVar SDelVar TDelVar UDelVar VDelVar WDelVar XDelVar YDelVar ZDelVar PDelVar theta");
+}
+*/
