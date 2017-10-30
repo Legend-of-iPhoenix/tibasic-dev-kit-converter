c56c7e221339ed552323d6b25a07d72d1c4d4e60
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-30 19:30:36 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function submitDirtyCode() {
 	var code = document.getElementById("dirty-code").value;
+	alert(code);
   
   if (code != '') {
     var input = document.getElementById("input");
@@ -18,7 +19,6 @@ function convertCode(codeToConvert) {
 
 function detectFormatGraphScreen(code)
 {
-	alert("1");
       var fixedCode = code.replace(/1:prgmthetaFMT/g,"RectGC:CoordOn:GridOff:AxesOn Black:LabelOff:ExprOn:BorderColor 1:DetectAsymOn");
       fixedCode = fixedCode.replace(/2:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4");
       fixedCode = fixedCode.replace(/3:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4:ZQuadrant1::264->Xmax:164->Ymax");
