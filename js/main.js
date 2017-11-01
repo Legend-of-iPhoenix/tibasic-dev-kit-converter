6cb80140ee654c2251e1db5a9a6abd33ad71c3ee
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-11-01 19:30:02 -0400

    Update main.js
    OMFG there must be a better way...

--- js/main.js
+++ js/main.js
@@ -34,8 +34,35 @@ function optimize(code) {
 	var fixedCode = code;
 	fixedCode = fixedCode.replace(/\u0029->/g,"->");
 	fixedCode = fixedCode.replace(/"->/g,"->");
-	fixedCode = fixedCode.replace(/0->[A-Z]/g,"DelVar ");
 	fixedCode = fixedCode.replace(/}->/g,"->");
+	
+	fixedCode = fixedCode.replace(/0->A\n/g,"DelVar A");
+	fixedCode = fixedCode.replace(/0->B\n/g,"DelVar B");
+	fixedCode = fixedCode.replace(/0->C\n/g,"DelVar C");
+	fixedCode = fixedCode.replace(/0->D\n/g,"DelVar D");
+	fixedCode = fixedCode.replace(/0->E\n/g,"DelVar E");
+	fixedCode = fixedCode.replace(/0->F\n/g,"DelVar F");
+	fixedCode = fixedCode.replace(/0->G\n/g,"DelVar G");
+	fixedCode = fixedCode.replace(/0->H\n/g,"DelVar H");
+	fixedCode = fixedCode.replace(/0->I\n/g,"DelVar I");
+	fixedCode = fixedCode.replace(/0->J\n/g,"DelVar J");
+	fixedCode = fixedCode.replace(/0->K\n/g,"DelVar K");
+	fixedCode = fixedCode.replace(/0->L\n/g,"DelVar L");
+	fixedCode = fixedCode.replace(/0->M\n/g,"DelVar M");
+	fixedCode = fixedCode.replace(/0->N\n/g,"DelVar N");
+	fixedCode = fixedCode.replace(/0->O\n/g,"DelVar O");
+	fixedCode = fixedCode.replace(/0->P\n/g,"DelVar P");
+	fixedCode = fixedCode.replace(/0->Q\n/g,"DelVar Q");
+	fixedCode = fixedCode.replace(/0->R\n/g,"DelVar R");
+	fixedCode = fixedCode.replace(/0->S\n/g,"DelVar S");
+	fixedCode = fixedCode.replace(/0->T\n/g,"DelVar T");
+	fixedCode = fixedCode.replace(/0->U\n/g,"DelVar U");
+	fixedCode = fixedCode.replace(/0->V\n/g,"DelVar V");
+	fixedCode = fixedCode.replace(/0->W\n/g,"DelVar W");
+	fixedCode = fixedCode.replace(/0->X\n/g,"DelVar X");
+	fixedCode = fixedCode.replace(/0->Y\n/g,"DelVar Y");
+	fixedCode = fixedCode.replace(/0->Z\n/g,"DelVar Z");
+	fixedCode = fixedCode.replace(/0->theta\n/g,"DelVar theta");
 	
 	fixedCode = fixedCode.replace(/DelVar A\n/g,"DelVar A");
 	fixedCode = fixedCode.replace(/DelVar B\n/g,"DelVar B");
