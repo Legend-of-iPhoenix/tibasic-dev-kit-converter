4730d43d227aa67ea6dace61d8efbb312adb74e7
Peter Tillema <petertillema@solcon.nl>
2017-11-04 19:53:46 +0100

    Clean code, easier to add new RegEx expressions
    Just extend the array at the very beginning; shouldn't be too hard to understand.

--- js/main.js
+++ js/main.js
@@ -1,100 +1,88 @@
+var optimizations = [
+	[/1:prgmthetaFMT/g,"RectGC:CoordOn:GridOff:AxesOn Black:LabelOff:ExprOn:BorderColor 1:DetectAsymOn"],
+	[/2:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4"],
+	[/3:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4:ZQuadrant1::264->Xmax:164->Ymax"],
+	[/prgmCLR0VAR/g,"DelVar ADelVar BDelVar CDelVar DDelVar EDelVar FDelVar GDelVar HDelVar IDelVar JDelVar KDelVar LDelVar MDelVar NDelVar ODelVar QDelVar RDelVar SDelVar TDelVar UDelVar VDelVar WDelVar XDelVar YDelVar ZDelVar PDelVar theta"],
+	[/\u0029->/g, "->"],
+	[/["}]->/g,"->"],
+	[/^0->A$/gm,"DelVar A"],
+	[/^0->B$/gm,"DelVar B"],
+	[/^0->C$/gm,"DelVar C"],
+	[/^0->D$/gm,"DelVar D"],
+	[/^0->E$/gm,"DelVar E"],
+	[/^0->F$/gm,"DelVar F"],
+	[/^0->G$/gm,"DelVar G"],
+	[/^0->H$/gm,"DelVar H"],
+	[/^0->I$/gm,"DelVar I"],
+	[/^0->J$/gm,"DelVar J"],
+	[/^0->K$/gm,"DelVar K"],
+	[/^0->L$/gm,"DelVar L"],
+	[/^0->M$/gm,"DelVar M"],
+	[/^0->N$/gm,"DelVar N"],
+	[/^0->O$/gm,"DelVar O"],
+	[/^0->P$/gm,"DelVar P"],
+	[/^0->Q$/gm,"DelVar Q"],
+	[/^0->R$/gm,"DelVar R"],
+	[/^0->S$/gm,"DelVar S"],
+	[/^0->T$/gm,"DelVar T"],
+	[/^0->U$/gm,"DelVar U"],
+	[/^0->V$/gm,"DelVar V"],
+	[/^0->W$/gm,"DelVar W"],
+	[/^0->X$/gm,"DelVar X"],
+	[/^0->Y$/gm,"DelVar Y"],
+	[/^0->Z$/gm,"DelVar Z"],
+	[/^0->theta$/gm,"DelVar theta"],
+	[/DelVar A\n/g,"DelVar A"],
+	[/DelVar B\n/g,"DelVar B"],
+	[/DelVar C\n/g,"DelVar C"],
+	[/DelVar D\n/g,"DelVar D"],
+	[/DelVar E\n/g,"DelVar E"],
+	[/DelVar F\n/g,"DelVar F"],
+	[/DelVar G\n/g,"DelVar G"],
+	[/DelVar H\n/g,"DelVar H"],
+	[/DelVar I\n/g,"DelVar I"],
+	[/DelVar J\n/g,"DelVar J"],
+	[/DelVar K\n/g,"DelVar K"],
+	[/DelVar L\n/g,"DelVar L"],
+	[/DelVar M\n/g,"DelVar M"],
+	[/DelVar N\n/g,"DelVar N"],
+	[/DelVar O\n/g,"DelVar O"],
+	[/DelVar P\n/g,"DelVar P"],
+	[/DelVar Q\n/g,"DelVar Q"],
+	[/DelVar R\n/g,"DelVar R"],
+	[/DelVar S\n/g,"DelVar S"],
+	[/DelVar T\n/g,"DelVar T"],
+	[/DelVar U\n/g,"DelVar U"],
+	[/DelVar V\n/g,"DelVar V"],
+	[/DelVar W\n/g,"DelVar W"],
+	[/DelVar X\n/g,"DelVar X"],
+	[/DelVar Y\n/g,"DelVar Y"],
+	[/DelVar Z\n/g,"DelVar Z"],
+	[/DelVar theta\n/g,"DelVar theta"]
+];
+
 function submitDirtyCode() {
 	var code = document.getElementById("dirty-code").value;
-	//in order to make my optimizations work, I need to delete all indentation, which is unfortunate.
-	code = code.replace(/\t/g,'');
-	//if they used spaces, alert them with a message telling them to use tabs, because the detection for that is simply too complicated.
-	//I'll add this soon enough x.x
-	code = "\n"+code;
-	document.getElementById("charCount1").innerHTML = "Length: "+code.length+" characters";
-	document.getElementById("charCount2").innerHTML = "Length: "+convertCode(code).length+" characters";
-  	
-  if (code = '')
-    alert("Please gimme code; I'm hungry :P");
-}
-
-function convertCode(codeToConvert) {
-  var cleanCode = optimize(detectClearVars(detectFormatGraphScreen(codeToConvert)));
-  document.getElementById("clean-code").value = cleanCode;
-  return cleanCode;
-}
-
-function detectFormatGraphScreen(code)
-{
-      var fixedCode = code;
-      fixedCode = fixedCode.replace(/1:prgmthetaFMT/g,"RectGC:CoordOn:GridOff:AxesOn Black:LabelOff:ExprOn:BorderColor 1:DetectAsymOn");
-      fixedCode = fixedCode.replace(/2:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4");
-      fixedCode = fixedCode.replace(/3:prgmthetaFMT/g,"GridOff:AxesOff:BorderColor 4:ZQuadrant1::264->Xmax:164->Ymax");
-      return fixedCode;
-}
-
-
-function detectClearVars(code) {
-  var fixedCode = code.replace(/prgmCLR0VAR/g,"DelVar ADelVar BDelVar CDelVar DDelVar EDelVar FDelVar GDelVar HDelVar IDelVar JDelVar KDelVar LDelVar MDelVar NDelVar ODelVar QDelVar RDelVar SDelVar TDelVar UDelVar VDelVar WDelVar XDelVar YDelVar ZDelVar PDelVar theta");
-  return fixedCode;
+	
+	// In order to make my optimizations work, I need to delete all indentation, which is unfortunate.
+	code = code.replace(/^\t+/g, "");
+	code = code.replace(/^ +/g, "");
+	
+	// Check if empty code
+	if (code == "") {
+		alert("Please gimme code; I'm hungry :P");
+	} else {
+		document.getElementById("charCount1").innerHTML = "Length: "+code.length+" characters";
+		var newCode = optimize(code);
+		document.getElementById("clean-code").value = newCode;
+		document.getElementById("charCount2").innerHTML = "Length: " + newCode.length + " characters";
+	}
 }
 
 function optimize(code) {
+	for (var index = 0; index < optimizations.length; index++) {
+		code = code.replace(optimizations[index][0], optimizations[index][1]);
+	}
 	
-	var fixedCode = code;
-	fixedCode = fixedCode.replace(/\u0029->/g,"->");
-	fixedCode = fixedCode.replace(/"->/g,"->");
-	fixedCode = fixedCode.replace(/}->/g,"->");
-	
-	fixedCode = fixedCode.replace(/\n^0->A$/gm,"DelVar A");
-	fixedCode = fixedCode.replace(/\n^0->B$/gm,"DelVar B");
-	fixedCode = fixedCode.replace(/\n^0->C$/gm,"DelVar C");
-	fixedCode = fixedCode.replace(/\n^0->D$/gm,"DelVar D");
-	fixedCode = fixedCode.replace(/\n^0->E$/gm,"DelVar E");
-	fixedCode = fixedCode.replace(/\n^0->F$/gm,"DelVar F");
-	fixedCode = fixedCode.replace(/\n^0->G$/gm,"DelVar G");
-	fixedCode = fixedCode.replace(/\n^0->H$/gm,"DelVar H");
-	fixedCode = fixedCode.replace(/\n^0->I$/gm,"DelVar I");
-	fixedCode = fixedCode.replace(/\n^0->J$/gm,"DelVar J");
-	fixedCode = fixedCode.replace(/\n^0->K$/gm,"DelVar K");
-	fixedCode = fixedCode.replace(/\n^0->L$/gm,"DelVar L");
-	fixedCode = fixedCode.replace(/\n^0->M$/gm,"DelVar M");
-	fixedCode = fixedCode.replace(/\n^0->N$/gm,"DelVar N");
-	fixedCode = fixedCode.replace(/\n^0->O$/gm,"DelVar O");
-	fixedCode = fixedCode.replace(/\n^0->P$/gm,"DelVar P");
-	fixedCode = fixedCode.replace(/\n^0->Q$/gm,"DelVar Q");
-	fixedCode = fixedCode.replace(/\n^0->R$/gm,"DelVar R");
-	fixedCode = fixedCode.replace(/\n^0->S$/gm,"DelVar S");
-	fixedCode = fixedCode.replace(/\n^0->T$/gm,"DelVar T");
-	fixedCode = fixedCode.replace(/\n^0->U$/gm,"DelVar U");
-	fixedCode = fixedCode.replace(/\n^0->V$/gm,"DelVar V");
-	fixedCode = fixedCode.replace(/\n^0->W$/gm,"DelVar W");
-	fixedCode = fixedCode.replace(/\n^0->X$/gm,"DelVar X");
-	fixedCode = fixedCode.replace(/\n^0->Y$/gm,"DelVar Y");
-	fixedCode = fixedCode.replace(/\n^0->Z$/gm,"DelVar Z");
-	fixedCode = fixedCode.replace(/\n^0->theta$/gm,"DelVar theta");
-	
-	fixedCode = fixedCode.replace(/DelVar A\n/g,"DelVar A");
-	fixedCode = fixedCode.replace(/DelVar B\n/g,"DelVar B");
-	fixedCode = fixedCode.replace(/DelVar C\n/g,"DelVar C");
-	fixedCode = fixedCode.replace(/DelVar D\n/g,"DelVar D");
-	fixedCode = fixedCode.replace(/DelVar E\n/g,"DelVar E");
-	fixedCode = fixedCode.replace(/DelVar F\n/g,"DelVar F");
-	fixedCode = fixedCode.replace(/DelVar G\n/g,"DelVar G");
-	fixedCode = fixedCode.replace(/DelVar H\n/g,"DelVar H");
-	fixedCode = fixedCode.replace(/DelVar I\n/g,"DelVar I");
-	fixedCode = fixedCode.replace(/DelVar J\n/g,"DelVar J");
-	fixedCode = fixedCode.replace(/DelVar K\n/g,"DelVar K");
-	fixedCode = fixedCode.replace(/DelVar L\n/g,"DelVar L");
-	fixedCode = fixedCode.replace(/DelVar M\n/g,"DelVar M");
-	fixedCode = fixedCode.replace(/DelVar N\n/g,"DelVar N");
-	fixedCode = fixedCode.replace(/DelVar O\n/g,"DelVar O");
-	fixedCode = fixedCode.replace(/DelVar P\n/g,"DelVar P");
-	fixedCode = fixedCode.replace(/DelVar Q\n/g,"DelVar Q");
-	fixedCode = fixedCode.replace(/DelVar R\n/g,"DelVar R");
-	fixedCode = fixedCode.replace(/DelVar S\n/g,"DelVar S");
-	fixedCode = fixedCode.replace(/DelVar T\n/g,"DelVar T");
-	fixedCode = fixedCode.replace(/DelVar U\n/g,"DelVar U");
-	fixedCode = fixedCode.replace(/DelVar V\n/g,"DelVar V");
-	fixedCode = fixedCode.replace(/DelVar W\n/g,"DelVar W");
-	fixedCode = fixedCode.replace(/DelVar X\n/g,"DelVar X");
-	fixedCode = fixedCode.replace(/DelVar Y\n/g,"DelVar Y");
-	fixedCode = fixedCode.replace(/DelVar Z\n/g,"DelVar Z");
-	fixedCode = fixedCode.replace(/DelVar theta\n/g,"DelVar theta");
-	
-	return fixedCode;
+	return code;
 }
